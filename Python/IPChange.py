e818c2f1342531e6be6a1ce53e42dd55e3d368ff
gizwag <andre@gizwag.co.za>
2017-04-29 17:29:08 +0200

    Added a Class and adjusted the code accordingly.

--- Python/IPChange.py
+++ Python/IPChange.py
@@ -1,26 +1,48 @@
 from tkinter import * # Imports the tkinter framework.
 
+
+
+class OptionButtons:
+
+
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.staticButton = Button(frame, text='Set Adapter to Static IP', command=self.setStaticIP)
+        self.staticButton.pack(side=LEFT)
+
+        self.staticButton = Button(frame, text='Set Adapter to Dynamic IP', command=self.setDHCP)
+        self.staticButton.pack(side=LEFT)
+
+        self.exitButton = Button(frame, text='Exit', command=frame.quit)
+        self.exitButton.pack()
+
+
+        self.msg = Message(master, text="")
+        self.msg.pack(side=LEFT)
+
+
+
+
+
+
+    def setStaticIP(self):
+       #print ("IP address has been made static")
+        txt = "I pressed Static"
+
+    def setDHCP(self):
+        print ("IP address has been made dynamic")
+
+
+
+
+
+
+
 root = Tk() # Creates a master window.
+buttons = OptionButtons(root)
+root.wm_title("LAN Adapter IP Changer") # Sets the windows title  bar to the app.
 root.geometry("420x340")
 
-topFrame = Frame(root)
-topFrame.pack()
-
-bottomFrame = Frame(root)
-bottomFrame.pack(side=BOTTOM)
-
-buttonStatic = Button(topFrame, text='Set Adapter to Static IP')
-buttonDHCP = Button(topFrame, text = 'Set Adapter to DHCP')
-
-
-
-buttonStatic.pack(side=LEFT)
-buttonDHCP.pack(side=LEFT)
-
-
-
-
-root.wm_title("LAN Adapter IP Changer") # Sets the windows title  bar to the app.
-
 root.mainloop() #Keeps the master window open until application is closed.
-
